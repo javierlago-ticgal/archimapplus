6f4aea956ddffb08b7b433369e67f7500d28ab94
Javier <javier.lago@ticgal.com>
2026-02-13 10:47:46 +0100

    change files to release

--- tools/generate_locales.sh
+++ tools/generate_locales.sh
@@ -4,19 +4,19 @@ CUR_PATH="`dirname \"$0\"`"
 
 cd "$CUR_PATH/.."
 
-xgettext *.php */*.php -o locales/moregroups.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po -k --keyword=__:1,2t --keyword=_x:1,2,3t --keyword=__s:1,2t --keyword=_sx:1,2,3t --keyword=_n:1,2,3,4t --keyword=_sn:1,2t --keyword=_nx:1,2,3t --copyright-holder "TICGAL"
+xgettext *.php */*.php -o locales/rcsangola.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po -k --keyword=__:1,2t --keyword=_x:1,2,3t --keyword=__s:1,2t --keyword=_sx:1,2,3t --keyword=_n:1,2,3,4t --keyword=_sn:1,2t --keyword=_nx:1,2,3t --copyright-holder "TICgal"
 
 cd locales
 
-sed -i "s/SOME DESCRIPTIVE TITLE/Gapp Extended Glpi Plugin/" moregroups.pot
-sed -i "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./TICGAL, $(date +%Y)/" moregroups.pot
-sed -i "s/YEAR/$(date +%Y)/" moregroups.pot
+sed -i "s/SOME DESCRIPTIVE TITLE/RCSANGOLA Glpi Plugin/" rcsangola.pot
+sed -i "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./TICgal, $(date +%Y)/" rcsangola.pot
+sed -i "s/YEAR/$(date +%Y)/" rcsangola.pot
 
 localazy upload
 localazy download
 
 for a in $(ls *.po); do
-	msgmerge -U $a moregroups.pot
+	msgmerge -U $a rcsangola.pot
 	msgfmt $a -o "${a%.*}.mo"
 done
 rm -f *.po~
